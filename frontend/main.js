7c844d015a303040217e26169ae435b936f00c0e
Cliford <cliffyangmia@gmail.com>
2024-11-19 10:39:22 +0100

    Updated Azure Function

---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionApiUrl = config.functionEndpoint;
+const functionApi = 'http://localhost:7071/api/GetVisitCounts';
+
+const getVisitCount = () => {
+    let count = 0;
+    fetch(functionApiUrl).then(response => {
+        console.log(response);
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count = response;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    })
+}
